50aea9368487372962e19014e4bfc4675e95da4f
richard <elms>
2020-02-28 16:09:52 +0100

    initial working version of android screenshots before cleanup and optimisation

--- android/src/main/java/com/applanga/applangaflutter/ApplangaFlutterPlugin.java
+++ android/src/main/java/com/applanga/applangaflutter/ApplangaFlutterPlugin.java
@@ -1,32 +1,84 @@
 package com.applanga.applangaflutter;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
 
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.view.FlutterView;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.embedding.engine.renderer.FlutterRenderer;
+
 import com.applanga.android.Applanga;
 import com.applanga.android.ApplangaCallback;
+import com.applanga.android.ApplangaScreenshotInterface;
+
 
 import java.util.HashMap;
 
 /**
  * ApplangaFlutterPlugin
  */
-public class ApplangaFlutterPlugin implements MethodCallHandler {
+public class ApplangaFlutterPlugin implements MethodCallHandler , FlutterPlugin, ActivityAware {
   
   private static Registrar registrar = null;
+
+  private static Object renderer;
+
+  // Condensed logic to initiliaze the plugin
+  private void initPlugin(Context context, BinaryMessenger messenger, Activity activity, Object renderer) {
+    Log.i("APPLANGA","renderer should be set");
+    renderer = renderer;
+  }
+
   /**
    * Plugin registration.
    */
   public static void registerWith(Registrar registrar) {
+    Log.println(Log.INFO, "APPLANGA", "registerWith PLUGIN REGISTERED");
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "applanga_flutter");
     channel.setMethodCallHandler(new ApplangaFlutterPlugin());
     ApplangaFlutterPlugin.registrar = registrar;
     Applanga.init(registrar.activeContext());
+    renderer = registrar.view();
+  }
+
+  private Bitmap getTheScreenshot()
+  {
+
+    try {
+      View view = registrar.activity().getWindow().getDecorView().getRootView();
+
+      view.setDrawingCacheEnabled(true);
+
+      Bitmap bitmap = null;
+      if( renderer.getClass().getSimpleName().equals("FlutterView") ) {
+        bitmap = ( (FlutterView) renderer ).getBitmap();
+      } else if( renderer.getClass().getSimpleName().equals("FlutterRenderer") ) {
+        bitmap = ( (FlutterRenderer) renderer ).getBitmap();
+      }
+
+      view.setDrawingCacheEnabled(false);
+
+      return bitmap;
+
+    } catch (Exception ex) {
+      Log.println(Log.INFO, "APPLANGA", "Error taking screenshot: " + ex.getMessage());
+    }
+
+    return null;
   }
 
   @Override
@@ -57,6 +109,14 @@ public class ApplangaFlutterPlugin implements MethodCallHandler {
           }
         }
       });
+
+      Applanga.setScreenshotInterface(new ApplangaScreenshotInterface(){
+          @Override
+        public Bitmap getScreenshot(){
+            return getTheScreenshot();
+          }
+      });
+
     } else if(call.method.equals("localizeMap")) {
       HashMap<String, HashMap<String, String>> map =
               (call.arguments instanceof HashMap<?,?>) ? (HashMap)call.arguments : new HashMap<>();
@@ -87,4 +147,60 @@ public class ApplangaFlutterPlugin implements MethodCallHandler {
       result.notImplemented();
     }
   }
+
+
+
+
+
+
+  // New v2 listener methods
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+   // this.context = null;
+  } // onDetachedFromEngine()
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+
+    initPlugin(
+            flutterPluginBinding.getApplicationContext(),
+            flutterPluginBinding.getBinaryMessenger(),
+            null,
+            flutterPluginBinding.getFlutterEngine().getRenderer()
+    ); // initPlugin()
+  } // onAttachedToEngine()
+
+
+  // Activity condensed methods
+  private void attachActivity(ActivityPluginBinding binding) {
+    //this.activity = binding.getActivity();
+  } // attachActivity()
+
+  private void detachActivity() {
+   // this.activity = null;
+  } // attachActivity()
+
+
+  // Activity listener methods
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding binding) {
+    attachActivity(binding);
+  } // onAttachedToActivity()
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+    detachActivity();
+  } // onDetachedFromActivityForConfigChanges()
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+    attachActivity(binding);
+  } // onReattachedToActivityForConfigChanges()
+
+  @Override
+  public void onDetachedFromActivity() {
+    detachActivity();
+  } // onDetachedFromActivity()
+
+
 }
