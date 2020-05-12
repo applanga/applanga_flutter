c38eda6f7ac477d598caa16c31c8244aec0e4b61
richard <elms>
2020-05-12 21:59:50 +0200

    adapted plugin to work with new flutter android api

--- android/src/main/java/com/applanga/applangaflutter/ApplangaFlutterPlugin.java
+++ android/src/main/java/com/applanga/applangaflutter/ApplangaFlutterPlugin.java
@@ -1,70 +1,127 @@
 package com.applanga.applangaflutter;
 
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.os.Handler;
 import android.util.Log;
+import android.view.PixelCopy;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 
-import androidx.annotation.NonNull;
 
-
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
-import io.flutter.view.FlutterView;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.embedding.engine.plugins.activity.ActivityAware;
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
-import io.flutter.plugin.common.BinaryMessenger;
-import io.flutter.embedding.engine.renderer.FlutterRenderer;
 
+import com.applanga.android.ALOverlay;
 import com.applanga.android.Applanga;
 import com.applanga.android.ApplangaCallback;
 import com.applanga.android.ApplangaScreenshotInterface;
 import com.applanga.android.ScreenshotCallback;
 
 
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * ApplangaFlutterPlugin
  */
-public class ApplangaFlutterPlugin implements MethodCallHandler, FlutterPlugin {
+public class ApplangaFlutterPlugin implements MethodCallHandler, FlutterPlugin, ActivityAware {
 
-  private static Registrar registrar = null;
+  private static Activity theActivity = null;
 
-  private static Object renderer;
+ private static Object mainFlutterView;
+
 
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "applanga_flutter");
     channel.setMethodCallHandler(new ApplangaFlutterPlugin());
-    ApplangaFlutterPlugin.registrar = registrar;
+    ApplangaFlutterPlugin.theActivity = registrar.activity();
     Applanga.init(registrar.activeContext());
-    renderer = registrar.view();
   }
+
+  @Override
+  public void onAttachedToEngine(FlutterPluginBinding binding) {
+    final MethodChannel channel = new MethodChannel(binding.getBinaryMessenger(), "applanga_flutter");
+    channel.setMethodCallHandler(new ApplangaFlutterPlugin());
+    Applanga.init(binding.getApplicationContext());
+  }
+
+  @Override
+  public void onDetachedFromEngine(FlutterPluginBinding binding) {
+
+  }
+
+  
 
   private Bitmap getTheScreenshot()
   {
     try {
-      View view = registrar.activity().getWindow().getDecorView().getRootView();
-      view.setDrawingCacheEnabled(true);
-      Bitmap bitmap = null;
-      if( renderer.getClass().getSimpleName().equals("FlutterView") ) {
-        bitmap = ( (FlutterView) renderer ).getBitmap();
-      } else if( renderer.getClass().getSimpleName().equals("FlutterRenderer") ) {
-        bitmap = ( (FlutterRenderer) renderer ).getBitmap();
-      }
-      view.setDrawingCacheEnabled(false);
-      return bitmap;
+        View rootView = theActivity.getWindow().getDecorView().getRootView();
+        rootView.setDrawingCacheEnabled(true);
+
+        ViewGroup theGroup = (ViewGroup)rootView;
+        FindTheFlutterView(theGroup);
+
+
+          Bitmap bitmap = null;
+
+        if(mainFlutterView instanceof io.flutter.view.FlutterView) {
+
+            bitmap = ((io.flutter.view.FlutterView) mainFlutterView).getBitmap();
+
+        }else if(mainFlutterView instanceof io.flutter.embedding.android.FlutterView) {
+
+            io.flutter.embedding.android.FlutterView theView = (io.flutter.embedding.android.FlutterView) mainFlutterView;
+            theView.setDrawingCacheEnabled(true);
+            theView.buildDrawingCache();
+            bitmap = theView.getDrawingCache();
+            theView.setDrawingCacheEnabled(false);
+
+        }
+
+        rootView.setDrawingCacheEnabled(false);
+        mainFlutterView = null;
+        return bitmap;
+
     } catch (Exception ex) {
       Log.println(Log.INFO, "APPLANGA", "Error taking screenshot: " + ex.getMessage());
     }
     return null;
   }
+
+  private void FindTheFlutterView(ViewGroup viewGroup)
+  {
+    for(int i = 0; i < viewGroup.getChildCount(); i++) {
+      View child = (View) viewGroup.getChildAt(i);
+      Log.println(Log.INFO, "APPLANGA", "CLASSNAME: " + child.getClass().getSimpleName());
+      if(child.getClass().getSimpleName().equals("FlutterView") )
+      {
+        mainFlutterView = child;
+        break;
+      }
+      if(child instanceof ViewGroup)
+      {
+        FindTheFlutterView((ViewGroup) child);
+      }
+    }
+  }
+
 
   @Override
   public void onMethodCall(MethodCall call, final Result result) {
@@ -90,8 +147,8 @@ public class ApplangaFlutterPlugin implements MethodCallHandler, FlutterPlugin {
       Applanga.captureScreenshot(tag,stringIds,useOcr,new ScreenshotCallback<Boolean>() {
         @Override
         public void execute(Boolean aBoolean) {
-          if(registrar != null){
-            registrar.activity().runOnUiThread(new Runnable() {
+          if(theActivity != null){
+            theActivity.runOnUiThread(new Runnable() {
               @Override
               public void run() {
                 Log.i("RICH", "Callback Called in java");
@@ -116,8 +173,8 @@ public class ApplangaFlutterPlugin implements MethodCallHandler, FlutterPlugin {
         @Override
         public void onLocalizeFinished(final boolean b) {
           Log.d("applanga", String.format("onLocalizeFinished(%b)", b));
-          if(registrar != null){
-            registrar.activity().runOnUiThread(new Runnable() {
+          if(theActivity != null){
+            theActivity.runOnUiThread(new Runnable() {
               @Override
               public void run() {
                 result.success(b);
@@ -144,12 +201,8 @@ public class ApplangaFlutterPlugin implements MethodCallHandler, FlutterPlugin {
     } else if(call.method.equals("setLanguage")) {
       result.success(Applanga.setLanguage(call.arguments.toString()));
     } else if(call.method.equals("showDraftModeDialog")) {
-      Activity activity = null;
-      if(registrar != null){
-        activity = registrar.activity();
-      }
-      if( activity != null ) {
-        Applanga.showDraftModeDialog(activity);
+      if( theActivity != null ) {
+        Applanga.showDraftModeDialog(theActivity);
         result.success(null);
       } else {
         result.error("DraftModeDialog", "Activity not found?", null);
@@ -166,12 +219,23 @@ public class ApplangaFlutterPlugin implements MethodCallHandler, FlutterPlugin {
   }
 
   @Override
-  public void onAttachedToEngine(FlutterPluginBinding binding) {
+  public void onAttachedToActivity(ActivityPluginBinding binding) {
+    theActivity = binding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
 
   }
 
   @Override
-  public void onDetachedFromEngine(FlutterPluginBinding binding) {
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+    theActivity = binding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
 
   }
+
 }
